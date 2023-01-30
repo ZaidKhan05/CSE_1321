9ea7b779afd8b6305483637bb6519c171b7e57e7
ZaidKhan <zaidkhan5213@gmail.com>
2023-01-30 12:52:03 -0500

    Lab 4a done

---
+++ CSE_1321L/Labs/Lab4/Lab4B.java
@@ -0,0 +1,46 @@
+//==================================== Program Lab4B.java ======================================
+/*
+Class: CSE 1321L
+Section: J51
+Term: Spring
+Instructor: Dmitri Nunes Dias Fernandes
+Name: Zaid Khan
+Lab# Lab 4b
+ */
+// Program Lab4B.java
+//
+import java.util.Scanner;
+
+public class Lab4B
+{
+    public static void main (String[] args)
+    {
+
+    }
+}
+/*
+Lab4B: Do I have class today?
+We’re going to create special messages for days of the week. For this lab, you will create a program that asks
+the user for a day of the week.
+• If the user is going to have class on Monday and Wednesday, but also is really excited for Friday.
+• Your program should respond with a special message for each of these days, keep in mind both Monday
+and Wednesday messages are the same and the user may or may not pay attention to capitalization.
+• For other days, the program will print the message “I should use this time to do my
+homework.”
+Make sure that the program is NOT case sensitive i.e., for example “Monday” and “monday” should
+lead to same result
+Remember, the class name should be Lab4B.
+Sample runs are shown below. The user input is in bold.
+Sample run #1:
+Enter the day: Monday
+I have class today!
+Sample run #2:
+Enter the day: monday
+I have class today!
+Sample run #3:
+Enter the day: Friday
+It’s Friday! Friday! Gotta get down on Friday!
+Sample run #4 (generic case):
+Enter the day: Tuesday
+I should use this time to do my homework.
+ */
---
+++ CSE_1321L/Notes/Labs/notepad.java
@@ -0,0 +1,18 @@
+//Sample run #1:
+//        Enter the score of your exam: 96
+//        Letter grade is: A
+/*
+A+ 98 - 100
+A 95 -97
+A- 92 - 94
+B+ 89 - 91
+B 86 - 88
+B- 83 - 85
+C+ 80 - 82
+C 77 -79
+C- 74 - 76
+D+ 71 - 73
+D 68 -70
+D- 65 - 67
+F 0 - 64
+ */
