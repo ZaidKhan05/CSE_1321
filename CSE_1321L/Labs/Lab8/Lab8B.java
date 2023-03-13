e3226d95e2854ccb6618fe3749d3d43e221a3fbb
ZaidKhan05 <zaidkhan5213@gmail.com>
2023-03-13 13:21:12 -0400

    lab 8 done

--- CSE_1321L/Labs/Lab8/Lab8B.java
+++ CSE_1321L/Labs/Lab8/Lab8B.java
@@ -10,12 +10,30 @@ Lab# Lab 8B
 // Program Lab8B.java
 //
 
-
+import java.util.Scanner;
 public class Lab8B
 {
     public static void main (String[] args)
     {
         //e
+        int rows, columns;
+        int count = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the number of rows: ");
+        rows = sc.nextInt();
+        System.out.print("Please enter the number of columns: ");
+        columns = sc.nextInt();
+        System.out.println("I have " + rows + " rows and " + columns + " columns. I need to fill-up " + (rows*columns) +" spaces.");
+        int[][] arraySpaces = new int [rows][columns];
+        System.out.println("The " + rows + "x" + columns + " array:");
+
+        for(int i = 0; i < arraySpaces.length; i++){
+            for(int j = 0; j <arraySpaces[0].length; j++){
+                System.out.print(count + "|");
+                count++;
+            }
+            System.out.println();
+        }
 
     }
 }
--- CSE_1321L/Labs/Lab8/Lab8C.java
+++ CSE_1321L/Labs/Lab8/Lab8C.java
@@ -10,12 +10,38 @@ Lab# Lab 8C
 // Program Lab8C.java
 //
 
+import java.util.Scanner;
+
 public class Lab8C
 {
     public static void main (String[] args)
     {
         //e
+        int rows, columns, area;
+        int count = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the number of rows: ");
+        rows = sc.nextInt();
+        System.out.print("Please enter the number of columns: ");
+        columns = sc.nextInt();
+        area = rows*columns;
+        System.out.println("I have " + rows + " rows and " + columns + " columns. I need to fill-up " + area +" spaces.");
+        int[][] arraySpaces = new int [rows][columns];
+        System.out.println("The " + rows + "x" + columns + " array:");
 
+        for(int i = 0; i < arraySpaces.length; i++){
+            for(int j = 0; j <arraySpaces[0].length; j++){
+                System.out.print(count + "|");
+                count++;
+            }
+            System.out.println();
+        }
+        System.out.println("The "+rows+"x"+columns+" 2-D array flattened into a "+area+" cell 1-D array: ");
+        count = 1;
+        for(int i = 0; i < area; i++){
+            System.out.print(count + "|");
+            count++;
+        }
     }
 }
 
