0623dec6aab24fa7b8a71aaca26148b6617891a8
ZaidKhan05 <zaidkhan5213@gmail.com>
2023-03-25 15:58:07 -0400

    assignment 5a done

--- CSE_1321L/Notes/Labs/notepad.java
+++ CSE_1321L/Notes/Labs/notepad.java
@@ -1,68 +1,108 @@
 /*
- Assignment 5 A: Hospital Directory. You’ve been asked to develop a directory look-up program
-for Community General Hospital. You will create multiple 1D arrays, predefined with the
-following information.
-ID First Name Last Name Age Occupation Has Medical License?
-0 Mark Sloan 67 Chief of Internal Medicine TRUE
-1 Amanda Bentley 33 Pathologist TRUE
-2 Norman Briggs 47 Administrator FALSE
-3 Steve Sloan 35 Hospital Security FALSE
-4 Jesse Travis 24 Surgeon Intern TRUE
-The ID column serves as a common index; the rest of the columns should be stored in individual
-arrays of the appropriate data type (especially important for the last column!).
-You will then use a loop to create a menu that repeatedly gives the user the following choices:
- List all employee records (in order)
- Display an employee record by ID
-◦ The user should enter a valid ID. If the employee exists, print out their
-information. Otherwise, tell the user that it is an invalid ID
- Display an employee record by last name
-◦ The user should enter a valid last name. If employee(s) exist, print out their
-information. Otherwise, tell the user that there are no records.
+Assignment 5 B: Art Program. We’re going to build on our PBM image creator from
+Assignment 4 to create a fully functional image editor, using 2D arrays to store our changes.
+We’ll also make it a little more interesting by switching to the Portable Gray Map standard. 8 bit
+PGM files have color values ranging from 0 (black) to 255 (white), with the values in-between
+being shades of gray. The header is very similar to PBM, and looks like this:
+P2
+WIDTH HEIGHT
+255
+IMAGE_DATA
+The P2 tells image readers that it is a PGM file, and the 255 indicates the largest possible color
+value is 255. Otherwise, it’s the same as Assignment 4.
+You will first prompt the user to enter a width, height, and initial color value to fill the image. You
+should then create a 2D array with those dimensions and initialize each cell with that color
+value.
+You will then use a loop to create a menu that repeatedly prompts the user to make one of the
+following choices:
+ Fill in a pixel with a new color
+◦ Prompt the user to enter a row and column value and a new color value. If the array
+has that row and column index AND the color value is between 0 – 255, change the
+color at the index.
+Otherwise, inform the user that it’s an invalid command.
+ Fill in a line with a new color
+◦ Prompt the user to enter a starting row and column value, a new color value, a length
+value, and a direction (left, right, up, or down, case sensitive).
+If all those values are valid, fill in each index in the line with those values in the 2D
+array (you can open up a program like Excel if you need help visualizing this)
+If the row, column, color, and/or direction are not correct, inform the user that it’s an
+invalid command.
+If the length is correct but would go outside the array (e.g. a length of 20 but the
+array only has 10 columns), fill in values up to the edge of the array and then stop.
+Do not cause an ArrayOutOfBounds error.
+ Print the PGM file
+◦ This should print out the contents of a valid PGM image. You should be able to copy
+and paste the output into a file and load it in the same way as you did with the PBM
+file in Assignment 4 – the only difference is that the extension should be .pgm.
  Quit
 Sample Output:
-[Community General Hospital Directory]
-1) List all employees
-2) Search employee records by ID
-3) Search employee records by Last Name
+[KSU Image Manipulation Program]
+Enter an image width: 5
+Enter an image height: 7
+Enter the fill color: 24
+What will you do?
+1) Fill in a pixel
+2) Fill in a line
+3) Print the image
 4) Quit
-Choice: 1
-#0: Mark Sloan, Age 67, Chief of Internal Medicine, Medical License:
-TRUE
-#1: Amanda Bentley, Age 33, Pathologist, Medical License: TRUE
-#2: Norman Briggs, Age 47, Administrator, Medical License: FALSE
-#3: Steven Sloan, Age 35, Hospital Security, Medical License: FALSE
-#4: Jesse Travis, Age 24, Surgeon Intern, Medical License: TRUE
-1) List all employees
-2) Search employee records by ID
-3) Search employee records by Last Name
+Choice? 3
+PGM Image Contents
+P2
+5 7
+255
+24 24 24 24 24
+24 24 24 24 24
+24 24 24 24 24
+24 24 24 24 24
+24 24 24 24 24
+24 24 24 24 24
+24 24 24 24 24
+What will you do?
+1) Fill in a pixel
+2) Fill in a line
+3) Print the image
 4) Quit
-Choice: 2
-ID: 10
-Invalid ID!
-ID: 1
-#1: Amanda Bentley, Age 33, Pathologist, Medical License: TRUE
-1) List all employees
-2) Search employee records by ID
-3) Search employee records by Last Name
+Choice? 1
+Row: 40
+Column: 2
+New Color: 23
+Invalid information!
+Row: 2
+Column: 1
+New Color: 123
+What will you do?
+1) Fill in a pixel
+2) Fill in a line
+3) Print the image
 4) Quit
-Choice: 3
-Last Name: Stewart
-No Records Found!
-1) List all employees
-2) Search employee records by ID
-3) Search employee records by Last Name
+Choice? 2
+Row: 5
+Column: 3
+New Color: 245
+Length: 6
+Direction: up
+What will you do?
+1) Fill in a pixel
+2) Fill in a line
+3) Print the image
 4) Quit
-Choice: 3
-Last Name: Sloan
-#0: Mark Sloan, Age 67, Chief of Internal Medicine, Medical License:
-TRUE
-#3: Steven Sloan, Age 35, Hospital Security, Medical License: FALSE
-1) List all employees
-2) Search employee records by ID
-3) Search employee records by Last Name
+Choice? 3
+PGM Image Contents
+P2
+5 7
+255
+24 24 24 245 24
+24 24 24 245 24
+24 123 24 245 24
+24 24 24 245 24
+24 24 24 245 24
+24 24 24 245 24
+24 24 24 24 24
+What will you do?
+1) Fill in a pixel
+2) Fill in a line
+3) Print the image
 4) Quit
-Choice: 5
-No option 5!
-Choice: 4
-[Closing Directory...]
+Choice? 4
+[Exiting KSU Image Manipulation Program]
 */
